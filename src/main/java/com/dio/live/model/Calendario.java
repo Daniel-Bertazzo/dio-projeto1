80df2a18f36478421e1c2030715c109bc04e2284
Daniel-Bertazzo <danielpcb610@gmail.com>
2021-08-03 20:04:40 -0300

    Parte 6

--- src/main/java/com/dio/live/model/Calendario.java
+++ src/main/java/com/dio/live/model/Calendario.java
@@ -2,15 +2,19 @@ package com.dio.live.model;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
 @Builder
-public class TipoData {
+public class Calendario {
 
     private long id;
+    private TipoData tipoData;
     private String descricao;
+    private LocalDateTime dataEspecial;
 
 }
