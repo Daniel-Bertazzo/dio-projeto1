2ad0254b8b335b9c293bbee3b75560c9dbb6b9bc
Daniel-Bertazzo <danielpcb610@gmail.com>
2021-08-04 00:18:19 -0300

    Parte 10

---
+++ src/main/java/com/dio/live/controller/JornadaController.java
@@ -0,0 +1,24 @@
+package com.dio.live.controller;
+
+import com.dio.live.model.JornadaTrabalho;
+import com.dio.live.service.JornadaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jornada")
+public class JornadaController {
+
+    // Autowired diretamente na variavel
+    @Autowired
+    JornadaService jornadaService;
+
+    @PostMapping
+    public JornadaTrabalho createJornada(@RequestBody JornadaTrabalho jornadaTrabalho) {
+        return jornadaService.saveJornada(jornadaTrabalho);
+    }
+
+}
